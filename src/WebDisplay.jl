c926c2af5ddfd27edb17d2741201494e81005fc2
Shashi Gowda <shashigowda91@gmail.com>
2016-12-14 13:08:45 +0530

    fix #2

--- src/WebDisplay.jl
+++ src/WebDisplay.jl
@@ -6,17 +6,6 @@ include("util.jl")
 
 function render end
 
-Base.show(io::IO, m::MIME"text/html", x) = show(io, m, render(x))
-
-function Base.mimewritable(m::MIME"text/html", x) # ¯\_(ツ)_/¯
-    applicable(render, x) ||
-        ismorespecific(
-            show,
-            (IO, typeof(m), typeof(x)),
-            (IO, typeof(m), Any)
-        )
-end
-
 include("node.jl")
 include("context.jl")
 
