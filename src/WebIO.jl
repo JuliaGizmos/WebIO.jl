8de0c4a90dca7a4b85e2d44888233bbc600f1e59
Shashi Gowda <shashi@juliacomputing.com>
2017-03-07 11:08:13 +0530

    setup function that detects UI

--- src/WebIO.jl
+++ src/WebIO.jl
@@ -20,4 +20,16 @@ function setup_provider(name)
     include(joinpath(dirname(@__FILE__), "providers", "$(name)_setup.jl"))
 end
 
+function setup()
+    if isdefined(Main, :IJulia)
+        setup_provider("ijulia")
+    elseif isdefined(Main, :Juno)
+        setup_provider("atom")
+    elseif isdefined(Main, :Blink)
+        setup_provider("blink")
+    elseif isdefined(Main, :Mux)
+        setup_provider("mux")
+    end
+end
+
 end # module
