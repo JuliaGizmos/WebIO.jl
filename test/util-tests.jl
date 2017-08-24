a04c1369fc2470f30d40f186f67d9ef5504ccd4a
Joel Mason <jobba1@hotmail.com>
2017-08-24 15:08:42 +1000

    Adds kebab2camel and camel2kebab
    Maybe these exist somewhere else? Was easier to implement them than to find them... :/

---
+++ test/util-tests.jl
@@ -0,0 +1,19 @@
+using Base.Test
+using WebIO
+
+import WebIO: kebab2camel, camel2kebab
+
+@testset "kebabs and camels" begin
+    kebabstrs = ["vue-instance-17-node-18", "bum-two-three", "silly nanny", "nanny"]
+    cameltrues = ["vueInstance17Node18", "bumTwoThree", "silly nanny", "nanny"]
+    camelstrs = kebab2camel.(kebabstrs)
+    for i in length(camelstrs)
+        @test camelstrs[i] == cameltrues[i]
+    end
+
+    kebabstrs = camel2kebab.(cameltrues)
+    kebab_also_trues = ["vue-instance17-node18", "bum-two-three", "silly nanny", "nanny"]
+    for i in length(kebabstrs)
+        @test camelstrs[i] == kebabstrs[i] || camelstrs[i] == kebab_also_trues[i]
+    end
+end
