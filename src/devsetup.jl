099414c0eca95c0b084a210b39e2eca80f1445cc
Shashi Gowda <shashi@juliacomputing.com>
2017-10-09 14:06:12 +0530

    fixup yarn setup

--- src/devsetup.jl
+++ src/devsetup.jl
@@ -1,14 +1,15 @@
 const ASSETS = joinpath(dirname(@__FILE__), "..", "assets")
+const YARN_VERSION = "1.1.0"
 function devsetup()
     cd(ASSETS) do
         if !isfile(".yarn/dist/bin/yarn")
             if !isfile("yarn.tar.gz")
-                download("https://yarnpkg.com/latest.tar.gz", "yarn.tar.gz")
+                download("https://yarnpkg.com/downloads/$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz", "yarn.tar.gz")
             end
             !isdir(".yarn") && mkdir(".yarn")
             run(`tar zvxf yarn.tar.gz -C .yarn`)
         end
-        run(`./.yarn/dist/bin/yarn install`)
+        run(`./.yarn/yarn-v$YARN_VERSION/bin/yarn install`)
     end
 end
 
