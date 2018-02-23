317944018b517e0008f785f8c827facbb0ab9bcb
Shashi Gowda <shashi@juliacomputing.com>
2018-02-23 05:57:17 +0530

    move JS files into a JupyterLab plugin structure

---
+++ assets/webio/jupyterlab_entry.js
@@ -0,0 +1,46 @@
+define(['./webio.js', '@jupyterlab/notebook'], function(WebIO, Notebook){
+
+    function activateWebIO(app, notebooks) {
+        notebooks.widgetAdded.connect(function (sender, panel) {
+           panel.context.session
+                .kernelChanged.connect(function (sender, kernel) {
+              newKernel(kernel);
+            });
+
+            if (panel.context.session.kernel) {
+              newKernel(context.session.kernel);
+            }
+        });
+
+        function newKernel(kernel) {
+            kernel.registerCommTarget('webio_comm', function (comm, commMsg) {
+                if (commMsg.content.target_name !== 'webio_comm') {
+                   return;
+                }
+
+                WebIO.triggerConnected()
+
+                WebIO.sendCallback = function (msg) {
+                    comm.send(msg)
+                }
+
+                comm.onMsg = function (msg) {
+                    WebIO.dispatch(msg.content.data);
+                }
+
+                comm.onClose = function (msg) {
+                  console.log(msg);  // 'bye'
+                }
+            })
+       }
+    }
+
+    return {
+        id: "jupyter.extensions.julia-web-io",
+        activate: activateWebIO,
+        autoStart: true,
+        requires: [Notebook.INotebookTracker],
+        load_ipython_extension: activateWebIO,
+        load_jupyter_extension: activateWebIO
+    }
+ });
