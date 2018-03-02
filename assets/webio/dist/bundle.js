f727a9d5e687d4b1c0c32dcca883d21480e899ef
Shashi Gowda <shashi@juliacomputing.com>
2018-03-02 09:00:20 +0530

    polyfill WebComponents

--- assets/webio/dist/bundle.js
+++ assets/webio/dist/bundle.js
@@ -2999,22 +2999,32 @@ module.exports = isArray || function (val) {
 /* 12 */
 /***/ (function(module, exports) {
 
-function evalInContext(js, context) {
-    return (function() { return eval(js); }).call(context);
-}
-if (document.createElement("unsafe-script").constructor === HTMLElement) {
-    var proto = Object.create(HTMLScriptElement.prototype);
-    proto.createdCallback = function() {
-        this.style.display = "none"
+(function() {
+    if ('registerElement' in document
+        && 'import' in document.createElement('link')
+        && 'content' in document.createElement('template')) {
+      // platform is good!
+    } else {
+      // polyfill the platform!
+      var e = document.createElement('script');
+      e.src = '/pkg/WebIO/webio/node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js';
+      document.body.appendChild(e);
     }
-    proto.attachedCallback = function() {
-        evalInContext(this.textContent, this)
+    function evalInContext(js, context) {
+        return (function() { return eval(js); }).call(context);
     }
+    if (document.createElement("unsafe-script").constructor === HTMLElement) {
+        var proto = Object.create(HTMLElement.prototype);
 
-    var UnsafeScript = document.registerElement("unsafe-script", {
-        prototype: proto
-    });
-}
+        proto.attachedCallback = function() {
+            evalInContext(this.textContent, this)
+        }
+
+        var UnsafeScript = document.registerElement("unsafe-script", {
+            prototype: proto
+        });
+    }
+})();
 
 
 /***/ }),
--- assets/webio/unsafe_script.js
+++ assets/webio/unsafe_script.js
@@ -1,14 +1,26 @@
-function evalInContext(js, context) {
-    return (function() { return eval(js); }).call(context);
-}
-if (document.createElement("unsafe-script").constructor === HTMLElement) {
-    var proto = Object.create(HTMLScriptElement.prototype);
-
-    proto.attachedCallback = function() {
-        evalInContext(this.textContent, this)
+(function() {
+    if ('registerElement' in document
+        && 'import' in document.createElement('link')
+        && 'content' in document.createElement('template')) {
+      // platform is good!
+    } else {
+      // polyfill the platform!
+      var e = document.createElement('script');
+      e.src = '/pkg/WebIO/webio/node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js';
+      document.body.appendChild(e);
     }
+    function evalInContext(js, context) {
+        return (function() { return eval(js); }).call(context);
+    }
+    if (document.createElement("unsafe-script").constructor === HTMLElement) {
+        var proto = Object.create(HTMLElement.prototype);
 
-    var UnsafeScript = document.registerElement("unsafe-script", {
-        prototype: proto
-    });
-}
+        proto.attachedCallback = function() {
+            evalInContext(this.textContent, this)
+        }
+
+        var UnsafeScript = document.registerElement("unsafe-script", {
+            prototype: proto
+        });
+    }
+})();
