565cbec65919a998095631d2bfff600fac27a2df
Shashi Gowda <shashi@juliacomputing.com>
2018-03-04 15:31:49 +0530

    fixup

--- assets/webio/unsafe_script.js
+++ assets/webio/unsafe_script.js
@@ -1,26 +1,39 @@
 (function() {
-    if ('registerElement' in document
+
+    var webComponentsSupported = ('customElements' in window
         && 'import' in document.createElement('link')
-        && 'content' in document.createElement('template')) {
-      // platform is good!
-    } else {
-      // polyfill the platform!
-      var e = document.createElement('script');
-      e.src = '/pkg/WebIO/webio/node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js';
-      document.body.appendChild(e);
-    }
+        && 'content' in document.createElement('template'));
+
     function evalInContext(js, context) {
         return (function() { return eval(js); }).call(context);
     }
-    if (document.createElement("unsafe-script").constructor === HTMLElement) {
-        var proto = Object.create(HTMLElement.prototype);
 
-        proto.attachedCallback = function() {
-            evalInContext(this.textContent, this)
+    function setup() {
+
+        class UnsafeScript extends HTMLElement {
+           constructor() {
+              super()
+           }
+           connectedCallback () {
+               evalInContext(this.textContent, this)
+           }
         }
 
-        var UnsafeScript = document.registerElement("unsafe-script", {
-            prototype: proto
-        });
+        window.customElements.define("unsafe-script", UnsafeScript);
+        console.log("defining unsafe-script")
+    }
+
+    if (!webComponentsSupported) {
+        var script = document.createElement('script');
+        script.async = true;
+        script.src = '/pkg/WebIO/webio/node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js';
+        document.head.appendChild(script);
+        document.addEventListener("WebComponentsReady", function () {
+            setup()
+        })
+    } else {
+        if (!(document.createElement("unsafe-script") instanceof HTMLElement)) {
+            setup()
+        }
     }
 })();
