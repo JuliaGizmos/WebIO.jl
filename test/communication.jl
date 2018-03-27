50d36f897ea62c8a08fd4123a8eb069a9f1e3673
Robin Deits <robin.deits@gmail.com>
2018-03-27 15:37:40 -0400

    fix order of operations in process_messages

--- test/communication.jl
+++ test/communication.jl
@@ -76,6 +76,49 @@ import WebIO: dispatch
                         "data"=>"hi Julia!", "scope"=>"testctx1"))
 
     @test msg[] == "hi Julia!"
+
+    # Test connection pool's handling of multiple simultaneous connections
+    @testset "connection pool" begin
+        @testset "sending to two connections" begin
+            outbox = Channel{Any}(1)
+            pool = WebIO.ConnectionPool(outbox)
+            t1 = TestConn(Channel{Any}(1))
+            t2 = TestConn(Channel{Any}(2))
+            # Both connections are added before the message is
+            # sent, so both should receive the message.
+            addconnection!(pool, t1)
+            # Previously, it was possible for `process_messages()`
+            # to advance to waiting for a new message rather than
+            # continuing to look for new connections if the current
+            # task yielded. We do so here to make sure it's fixed.
+            yield()            
+            addconnection!(pool, t2)
+            put!(outbox, "hello")
+            @test take!(t1.channel) == "hello"
+            @test take!(t2.channel) == "hello"
+        end
+
+        @testset "sending to one connection" begin
+            outbox = Channel{Any}(1)
+            pool = WebIO.ConnectionPool(outbox)
+            t1 = TestConn(Channel{Any}(1))
+            t2 = TestConn(Channel{Any}(2))
+            # Only t1 has been added, so only it should receive
+            # the message:
+            addconnection!(pool, t1)
+            put!(outbox, "hello1")
+            @test take!(t1.channel) == "hello1"
+            @test !isready(t2.channel)
+
+            # Now we add a second connection and make sure both
+            # connections get all future messages
+            addconnection!(pool, t2)
+            put!(outbox, "hello2")
+            @test take!(t1.channel) == "hello2"
+            @test take!(t2.channel) == "hello2"
+        end
+    end
+
 end
 
 
