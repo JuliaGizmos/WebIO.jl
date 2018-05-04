cdcd4eaf7b435ec4fa931504d432c8f101a50a33
Robin Deits <robin.deits@gmail.com>
2018-05-04 13:00:37 -0400

    remove devsetup entirely

--- deps/_bundlejs.jl
+++ deps/_bundlejs.jl
@@ -5,23 +5,20 @@ cd(Pkg.dir()) do
     Pkg.Entry.resolve(merge(Pkg.Reqs.parse("REQUIRE"), reqs))
 end
 
-cwd = pwd()
-
-cd(joinpath(dirname(@__FILE__), "..", "assets", "webio"))
-
 using NodeJS
-node = NodeJS.nodejs_cmd()
-npm = NodeJS.npm_cmd()
 
-run(`$npm install --scripts-prepend-node-path`)
-if haskey(ENV, "WEBIO_WEBPACK_ARGS")
-    args = [ENV["WEBIO_WEBPACK_ARGS"]]
-else
-    args =[]
+cd(joinpath(dirname(@__FILE__), "..", "assets", "webio")) do
+    node = NodeJS.nodejs_cmd()
+    npm = NodeJS.npm_cmd()
+
+    run(`$npm install --scripts-prepend-node-path`)
+    if haskey(ENV, "WEBIO_WEBPACK_ARGS")
+        args = [ENV["WEBIO_WEBPACK_ARGS"]]
+    else
+        args =[]
+    end
+    run(`$npm run build --scripts-prepend-node-path -- $args`)
 end
-run(`$node ../../deps/node_modules/webpack-cli/bin/webpack.js $args`)
-
-cd(cwd)
 
 cd(Pkg.dir()) do
     Pkg.Entry.resolve() # Uninstall NodeJS if it wasn't there before
