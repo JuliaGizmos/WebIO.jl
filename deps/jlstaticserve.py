5f79992344417bc5a081fb299ea6dac99f8136db
Shashi Gowda <shashi@juliacomputing.com>
2018-06-15 13:44:06 +0530

    use assetregistry in Jupyter plugin

--- deps/jlstaticserve.py
+++ deps/jlstaticserve.py
@@ -18,17 +18,40 @@ class JuliaPackageAssetServer(IPythonHandler, StaticFileHandler):
 
     @gen.coroutine
     def get(self, pkg, fpath):
-        dirs = self.julia_load_path
-        for d in dirs:
-            dirpath = os.path.join(d, pkg, "assets")
-            fullpath = os.path.join(dirpath, fpath)
-            if os.path.isdir(dirpath) and os.path.isfile(fullpath):
-                self.root = dirpath
-                yield StaticFileHandler.get(self, fpath)
-                break
+        if pkg != "assetserver":
+            # old server
+            dirs = self.julia_load_path
+            for d in dirs:
+                dirpath = os.path.join(d, pkg, "assets")
+                fullpath = os.path.join(dirpath, fpath)
+                if os.path.isdir(dirpath) and os.path.isfile(fullpath):
+                    self.root = dirpath
+                    yield StaticFileHandler.get(self, fpath)
+                    break
+            else:
+                # will execute only if the `for` loop never `break`s
+                raise HTTPError(404)
         else:
-            # will execute only if the `for` loop never `break`s
-            raise HTTPError(404)
+            homedir = os.path.expanduser("~")
+            data = {}
+            registry = os.path.join(homedir, ".jlassetregistry.json")
+            if not os.path.isfile(registry):
+                raise HTTPError(404)
+            with open(registry, "r") as assets_file:
+                data = json.load(assets_file)
+            parts = fpath.split("/", 1)
+            if len(parts) > 1:
+                sha = parts[0]
+            else:
+                sha = fpath
+            key = "/assetserver/" + sha
+            if key in data:
+                f = data[key][0]
+                if len(parts) > 1:
+                    f = os.path.join(f, parts[1])
+                yield StaticFileHandler.get(self, f)
+            else:
+                raise HTTPError(404)
 
 def load_jupyter_server_extension(nb_server_app):
     """
@@ -36,9 +59,13 @@ def load_jupyter_server_extension(nb_server_app):
     """
     web_app = nb_server_app.web_app
     host_pattern = '.*$'
-    route_pattern = url_path_join(web_app.settings['base_url'], '/pkg/([^/]+)/(.*)$')
+    route_pattern_old = url_path_join(web_app.settings['base_url'], '/pkg/([^/]+)/(.*)$')
+    route_pattern = url_path_join(web_app.settings['base_url'], '/(assetserver)/(.*)$')
     with open(os.path.join(os.path.dirname(__file__), "load_paths.json"), "r") as paths_file:
         JuliaPackageAssetServer.julia_load_path = json.load(paths_file)
     print("Will serve asset files from any Julia package under", JuliaPackageAssetServer.julia_load_path)
+    web_app.add_handlers(
+            host_pattern, [(route_pattern_old, JuliaPackageAssetServer)])
+
     web_app.add_handlers(
             host_pattern, [(route_pattern, JuliaPackageAssetServer)])
