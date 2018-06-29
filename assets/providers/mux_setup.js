98ec4bf177ea706a5a0590ddd3f07feff6a4a6ea
Shashi Gowda <shashi@juliacomputing.com>
2018-06-29 10:04:12 +0530

    try upper level locations in Mux WebSocket connection till we reach the WebIO server root

--- assets/providers/mux_setup.js
+++ assets/providers/mux_setup.js
@@ -1,5 +1,5 @@
 (function () {
-    function wsurl(suffix) {
+    function wsurl() {
         var loc = window.location, new_uri;
         if (loc.protocol === "https:") {
                 new_uri = "wss:";
@@ -7,18 +7,39 @@
                 new_uri = "ws:";
         }
         new_uri += "//" + loc.host;
-        new_uri += loc.pathname + suffix;
+        new_uri += loc.pathname;
         return new_uri;
     }
 
-    var ws = new WebSocket(wsurl("/webio-socket"));
-    ws.onopen = function () {
-        WebIO.triggerConnected();
+    function tryconnect(url) {
+
+        var ws = new WebSocket(url + "/webio-socket");
+        ws.onopen = function () {
+            WebIO.sendCallback = function (msg) {
+                ws.send(JSON.stringify(msg))
+            }
+
+            ws.onmessage = function (evt) {
+                WebIO.dispatch(JSON.parse(evt.data));
+            }
+
+            WebIO.triggerConnected()
+        }
+        ws.onclose = function (evt) {
+            if (evt.code === 1005) {
+                // no status code recv -- happens when 404
+                url = url.replace(/\/+$/, "")
+                url = url.match(/.*\//)[0]; // dirname
+                url = url.replace(/\/+$/, "")
+                if (url === "http:" || url === "https:") {
+                    // we tried everything!
+                    return
+                } else {
+                    tryconnect(url)
+                }
+            }
+        }
     }
-    WebIO.sendCallback = function (msg) {
-        ws.send(JSON.stringify(msg))
-    }
-    ws.onmessage = function (evt) {
-        WebIO.dispatch(JSON.parse(evt.data));
-    }
+
+    tryconnect(wsurl())
 })();
