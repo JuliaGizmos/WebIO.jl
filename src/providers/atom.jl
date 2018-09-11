42897451b277c05828d5668f1fba6f2485041fb3
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2018-09-11 15:59:06 +0200

    better mux implementation

--- src/providers/atom.jl
+++ src/providers/atom.jl
@@ -2,6 +2,7 @@ using Logging
 
 const pages = Dict{String,Any}()
 const serving = Ref{Bool}(false)
+const port = Ref{Int}(9000)
 
 function routepages(req)
     return pages[req[:params][:id]]
@@ -21,29 +22,44 @@ function Base.show(io::IO, ::MIME"application/juno+plotpane", n::Union{Node, Sco
     id = rand(UInt128)
     pages[string(id)] = n
 
-    port = rand(8000:9000)
-
     if !serving[]
-        # hide http logging messages
-        with_logger(NullLogger()) do
-            @async begin
-                http = Mux.App(Mux.mux(
-                    Mux.defaults,
-                    Mux.route("/:id", routepages),
-                    Mux.notfound()
-                ))
+        setup_server()
+    end
+    print(io, "<meta http-equiv=\"refresh\" content=\"0; url=http://localhost:$(port[])/$(id)\"/>")
+end
 
-                websock = Mux.App(Mux.mux(
-                    Mux.wdefaults,
-                    Mux.route("/webio-socket", create_silent_socket),
-                    Mux.wclose,
-                    Mux.notfound(),
-                ))
+function setup_server()
+    port[] = rand(8000:9000)
 
-                Mux.serve(http, websock, port)
-                serving[] = true
+    # hide http logging messages
+    with_logger(NullLogger()) do
+        @async begin
+            http = Mux.App(Mux.mux(
+                Mux.defaults,
+                Mux.route("/:id", routepages),
+                Mux.notfound()
+            ))
+
+            websock = Mux.App(Mux.mux(
+                Mux.wdefaults,
+                Mux.route("/webio-socket", create_silent_socket),
+                Mux.wclose,
+                Mux.notfound(),
+            ))
+
+            for i = 1:100
+                iserr = false
+                try
+                    Mux.serve(http, websock, port[])
+                catch err
+                    iserr = true
+                    port[] = rand(8000:9000)
+                end
+                iserr || break
             end
+            serving[] = true
         end
     end
-    print(io, "<meta http-equiv=\"refresh\" content=\"0; url=http://localhost:$(port)/$(id)\"/>")
 end
+
+WebIO.setup_provider(::Val{:juno}) = setup_server()
