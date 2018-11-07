c1319fd161ff7b4852da6a2426a28e86e427d669
SimonDanisch <sdanisch@gmail.com>
2018-11-07 15:22:27 +0100

    fix tests

--- test/http-tests.jl
+++ test/http-tests.jl
@@ -7,53 +7,6 @@ using Test
     @test occursin("_webIOWebSocketURL = ", output)
     @test occursin("ws://127.0.0.1:8081/webio_websocket/", output)
     @test occursin("""hello, world""", output)
-    @test WebIO.webio_server_config[] == (url = "127.0.0.1", http_port = 8081, ws_url = "ws://127.0.0.1:8081/webio_websocket/")
+    @test WebIO.webio_server_config[] == (url = "127.0.0.1", bundle_url = WebIO.bundle_key, http_port = 8081, ws_url = "ws://127.0.0.1:8081/webio_websocket/")
     @test isassigned(WebIO.singleton_instance)
 end
-
-
-using WebSockets, WebIO
-
-app = Ref{Any}(node(:div, "hi"))
-function serve_app(req)
-    if req.target == "/"
-        return sprint() do io
-            print(io,
-                """
-                <!doctype html>
-                <html>
-                <head>
-                <meta charset="UTF-8">
-                </head>
-                <body>
-                """
-            )
-            show(io, WebIO.WEBIO_APPLICATION_MIME(), app[])
-            print(io,
-                """
-                </body>
-                </html>
-                """
-            )
-        end
-    else
-        return missing
-    end
-end
-server = WebIO.WebIOServer(serve_app, logger = stdout, verbose = true)
-server.serve_task
-
-w = Scope()
-
-obs = Observable(w, "rand-value", 0.0)
-
-on(obs) do x
-   println("JS sent $x")
-end
-
-app[] = w(
- dom"button"(
-   "generate random",
-   events=Dict("click" => js"""function() { _webIOScope.setObservableValue('rand-value', Math.random()); }"""),
- ),
-);
