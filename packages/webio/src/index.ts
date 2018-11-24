3e5930a523ee41834ea2b7363268b28221b64dbf
Travis DePrato <travigd@umich.edu>
2018-11-24 16:30:23 -0500

    Re-export everything from index.

--- packages/webio/src/index.ts
+++ packages/webio/src/index.ts
@@ -1,3 +1,16 @@
 import WebIO from "./WebIO";
-export {NODE_CLASSES} from "./createNode";
 export default WebIO;
+
+export * from "./DomNode";
+export * from "./IFrame";
+export * from "./Node";
+export * from "./Observable";
+export * from "./ObservableNode";
+export * from "./Scope";
+export * from "./WebIO";
+export * from "./createNode";
+export * from "./events";
+export * from "./imports";
+export * from "./message";
+export * from "./setInnerHTML";
+export * from "./utils";
