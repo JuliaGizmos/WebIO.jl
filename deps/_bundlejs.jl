7bed5c9c31aeb6638715bebc0530e3e349885ada
Travis DePrato <travigd@umich.edu>
2019-02-08 16:29:30 -0500

    Why does CI fail?????

--- deps/_bundlejs.jl
+++ deps/_bundlejs.jl
@@ -4,11 +4,18 @@ cd(joinpath(dirname(@__FILE__), "..", "packages")) do
     node = NodeJS.nodejs_cmd()
     npm = NodeJS.npm_cmd()
 
+    @info "Running `npm install`..."
     run(`$npm install --scripts-prepend-node-path=auto --unsafe-perm`)
+    @info "Completed `npm install`."
+    run(`ls node_modules`)
+    run(`ls webio/node_modules`)
     if haskey(ENV, "WEBIO_WEBPACK_ARGS")
         args = [ENV["WEBIO_WEBPACK_ARGS"]]
     else
         args =[]
     end
+
+    @info "Running `npm run build-prod`..."
     run(`$npm run build-prod --scripts-prepend-node-path=auto --unsafe-perm -- $args`)
+    @info "Completed `npm run build-prod`."
 end
