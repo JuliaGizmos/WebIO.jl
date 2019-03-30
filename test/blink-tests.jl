211806e8ac547e1e36c708efc66abfbd3b8f7d74
Travis DePrato <travigd@umich.edu>
2019-03-30 14:50:35 -0400

    Simplify RPC API.

--- test/blink-tests.jl
+++ test/blink-tests.jl
@@ -23,20 +23,8 @@ function with_timeout(f::Function, timeout)
     take!(c)
 end
 
-"""
-    open_window()
-
-Open a window, optionally showing it when the BLINK_DEBUG environment variable
-is set (to allow for seeing what happens in the Electron console).
-"""
-function open_window()
-    if haskey(ENV, "BLINK_DEBUG")
-        w = Window(Dict(:show => true))
-        @js w localStorage.debug = "*"
-        opentools(w)
-        return w
-    end
-    return Window(Dict(:show => false))
+if !(@isdefined open_window)
+    include("./test-utils.jl")
 end
 
 # IMPORTANT: Cannot open Window's inside of @testsets.
