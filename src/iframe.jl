67450830e194fdbcedbd9b3bd0ac26d98a425d33
Travis DePrato <travigd@umich.edu>
2019-04-05 23:11:19 -0400

    Wrap iframes in a scope.

--- src/iframe.jl
+++ src/iframe.jl
@@ -9,7 +9,16 @@ iframe_bundle_key() = AssetRegistry.register(normpath(joinpath(
     WebIO.packagepath, "generic-http-provider", "dist", "generic-http.js"
 )))
 
-iframe(content) = node(IFrame(
+"""
+    iframe(content)
+
+Wrap the content in an IFrame.
+
+Content is typically a WebIO node, but may be anything with a `text/html` MIME
+representation. The IFrame itself is wrapped in a scope to allow for mount
+callbacks.
+"""
+iframe(content) = Scope()(node(IFrame(
     stringmime("text/html", content),
     string(WebIO.baseurl[], iframe_bundle_key()),
-))
+)))
