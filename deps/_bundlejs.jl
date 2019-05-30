a9c7a15cdb763cb2dcfbfc5466515a7789fe9829
Travis Garvin Deprato <travisd@fb.com>
2019-05-29 18:45:16 -0700

    Only run bundlejs if checkout out for dev.

--- deps/_bundlejs.jl
+++ deps/_bundlejs.jl
@@ -1,14 +1,26 @@
 using NodeJS
 
-cd(joinpath(dirname(@__FILE__), "..", "packages")) do
-    node = NodeJS.nodejs_cmd()
-    npm = NodeJS.npm_cmd()
+# Wrap in useless try/catch to avoid namespace pollution
+try
+    isdev = basename(dirname(dirname(dirname(@__FILE__)))) == "dev"
+    if haskey(ENV, "CI") || isdev
+        cd(joinpath(dirname(@__FILE__), "..", "packages")) do
+            npm = NodeJS.npm_cmd()
 
-    run(`$npm install --scripts-prepend-node-path=auto --unsafe-perm`)
-    if haskey(ENV, "WEBIO_WEBPACK_ARGS")
-        args = [ENV["WEBIO_WEBPACK_ARGS"]]
+            run(`$npm install --scripts-prepend-node-path=auto --unsafe-perm`)
+            if haskey(ENV, "WEBIO_WEBPACK_ARGS")
+                args = [ENV["WEBIO_WEBPACK_ARGS"]]
+            else
+                args =[]
+            end
+            run(`$npm run build-prod --scripts-prepend-node-path=auto --unsafe-perm -- $args`)
+        end
     else
-        args =[]
+        @warn(
+            "Can't build WebIO JS when not checkout out for development. "
+            * "Try running Pkg.dev(\"WebIO\") if you want to rebuild JS."
+        )
     end
-    run(`$npm run build-prod --scripts-prepend-node-path=auto --unsafe-perm -- $args`)
+catch
+    rethrow()
 end
