8353da3c574beadc8012e5de5b7f663b5948351f
Travis DePrato <773453+travigd@users.noreply.github.com>
2019-06-09 23:31:38 -0700

    Fix bad deprecation for Scope constructor. (#308)

--- test/deprecations.jl
+++ test/deprecations.jl
@@ -4,4 +4,8 @@ using Test
 @testset "Scope id deprecations" begin
     @test_deprecated Scope("manual-ids-are-deprecated")
     @test_deprecated Scope(; id="still-deprecated")
+
+    # Make sure that non-deprecated arguments are still handled as expected.
+    # This surfaced in https://github.com/JuliaGizmos/Interact.jl/issues/315.
+    @test_deprecated !isempty(Scope("foo"; imports=["foo.js"]).imports)
 end
