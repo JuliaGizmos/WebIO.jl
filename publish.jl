47602e3d372a9e1278cc6e6b540d7ee18389582f
Travis Garvin DePrato <travigd@umich.edu>
2019-07-23 14:55:25 -0700

    Finish publish.jl script.

--- publish.jl
+++ publish.jl
@@ -8,6 +8,17 @@ const PROJECT = TOML.parsefile(PROJECT_FILENAME)
 const CURRENT_VERSION = VersionNumber(PROJECT["version"])
 major, minor, patch = Int.(getfield.(CURRENT_VERSION, [:major, :minor, :patch]))
 
+const NPM_OTP = get(ENV, "NPM_OTP", "")
+if NPM_OTP == ""
+    # We require a one-time-password to publish packages to NPM.
+    @error(
+		"A one-time-password is required to publish packages in the @webio org.\n"
+        * "Please specify the NPM_OTP environment variable:\n"
+        * "    NPM_OTP=123456 ./publish.jl"
+    )
+    exit(1)
+end
+
 if length(ARGS) == 0 || ARGS[1] == "bump"
     target_version = VersionNumber("$(major).$(minor).$(patch + 1)")
     @info "Bumping WebIO version: $CURRENT_VERSION => $target_version"
@@ -36,5 +47,4 @@ Pkg.test("WebIO")
 
 @info "Publishing NPM packages via Lerna..."
 cd(PACKAGES_DIR)
-lerna_args = split(get(ENV, "LERNA_ARGS", ""))
-run(`npm run lerna -- publish $lerna_args $target_version`)
+run(`npm run lerna -- publish --otp $NPM_OTP $target_version`)
