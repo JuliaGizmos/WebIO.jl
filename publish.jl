2a64acf517789612e1b8a48af81034cd0b9b8a9e
Travis Garvin DePrato <travigd@umich.edu>
2019-07-24 09:02:26 -0700

    Update publish.jl script (ask for OTP right before publish).

--- publish.jl
+++ publish.jl
@@ -8,17 +8,6 @@ const PROJECT = TOML.parsefile(PROJECT_FILENAME)
 const CURRENT_VERSION = VersionNumber(PROJECT["version"])
 major, minor, patch = Int.(getfield.(CURRENT_VERSION, [:major, :minor, :patch]))
 
-const NPM_OTP = get(ENV, "NPM_OTP", "")
-if NPM_OTP == ""
-    # We require a one-time-password to publish packages to NPM.
-    @error(
-		"A one-time-password is required to publish packages in the @webio org.\n"
-        * "Please specify the NPM_OTP environment variable:\n"
-        * "    NPM_OTP=123456 ./publish.jl"
-    )
-    exit(1)
-end
-
 if length(ARGS) == 0 || ARGS[1] == "bump"
     target_version = VersionNumber("$(major).$(minor).$(patch + 1)")
     @info "Bumping WebIO version: $CURRENT_VERSION => $target_version"
@@ -38,7 +27,7 @@ end
 
 const PACKAGES_DIR = normpath(joinpath(@__DIR__, "packages"))
 @info "Removing previous build artifacts..."
-run(`sh -c "rm -rf ./deps/bundles $(PACKAGES_DIR)/node_modules $(PACKAGES_DIR)/*/node_modules $(PACKAGES_DIR)/*/dist"`)
+run(`sh -c "rm -rf ./deps/bundles $(PACKAGES_DIR)/node_modules $(PACKAGES_DIR)/*/node_modules $(PACKAGES_DIR)/*/dist $(PACKAGES_DIR)/package-lock.json $(PACKAGES_DIR)/*/package-lock.json"`)
 
 @info "Running tests..."
 # Build JS in prod mode.
@@ -46,5 +35,7 @@ ENV["WEBIO_BUILD_PROD"] = true
 Pkg.test("WebIO")
 
 @info "Publishing NPM packages via Lerna..."
+print("Please enter your NPM one-time-password (OTP): ")
+otp = readline()
 cd(PACKAGES_DIR)
-run(`npm run lerna -- publish --otp $NPM_OTP $target_version`)
+run(`npm run lerna -- publish --otp $otp $target_version`)
