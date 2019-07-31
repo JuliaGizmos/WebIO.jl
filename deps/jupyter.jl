5fbd47010c68b2e820f268793cc750c65e3095c4
Travis Garvin DePrato <travigd@umich.edu>
2019-07-31 14:43:51 -0700

    Don't error on jupyterlab unlink.

--- deps/jupyter.jl
+++ deps/jupyter.jl
@@ -5,6 +5,8 @@ end
 const CONFIG_BEGIN_MARKER = "###JULIA-WEBIO-CONFIG-BEGIN"
 const CONFIG_END_MARKER = "###JULIA-WEBIO-CONFIG-END"
 
+_tryrun(args...; kwargs...) = try run(args...; kwargs...) catch nothing end
+
 """
     install_notebook_config()
 
@@ -144,8 +146,8 @@ function install_jupyter_labextension(
     end
     install_jupyter_serverextension()
 
-    run(`$jupyter labextension unlink --no-build @webio/webio`)
-    run(`$jupyter labextension uninstall --no-build @webio/jupyter-lab-provider`)
+    _tryrun(`$jupyter labextension unlink --no-build @webio/webio`)
+    _tryrun(`$jupyter labextension uninstall --no-build @webio/jupyter-lab-provider`)
 
     if dev
         @info "Installing Jupyter labextension in dev mode..."
