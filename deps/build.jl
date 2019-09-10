5e236e34b6c5047393ca9e4ac861203ec3abf37a
Jeff Fessler <JeffFessler@users.noreply.github.com>
2019-09-10 18:52:56 -0400

    Add missing word in warning

--- deps/build.jl
+++ deps/build.jl
@@ -8,7 +8,7 @@ download_js_bundles()
 # See https://github.com/JuliaGizmos/WebIO.jl/issues/314 for the rational behind
 # why we're not installing Jupyter packages by default anymore.
 @warn(
-    "WebIO no longer Jupyter extensions automatically; please run "
+    "WebIO no longer installs Jupyter extensions automatically; please run "
     * "`WebIO.install_jupyter_notebook()` or `WebIO.install_jupyter_lab()` if "
     * "needed."
 )
