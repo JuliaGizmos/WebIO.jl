74a0651e80d0a934c5139ae63639b1e27437aebb
Shashi Gowda <shashigowda91@gmail.com>
2019-12-16 17:17:15 -0500

    RFC: Synchronous updates by default

--- src/connection.jl
+++ src/connection.jl
@@ -5,11 +5,6 @@ export AbstractConnection
 abstract type AbstractConnection end
 
 """
-The maximum number of messages to allow into the outbox.
-"""
-const DEFAULT_OUTBOX_LIMIT = 32
-
-"""
     ConnectionPool([outbox[, connections]])
 
 Manages the distribution of messages from the `outbox` channel to a set of
@@ -18,62 +13,31 @@ and sends each message to every connection in the pool. Any closed connections
 are automatically removed from the pool.
 """
 struct ConnectionPool
-    outbox::Channel
+    outbox::Vector
     connections::Set{AbstractConnection}
-    condition::Condition
 end
 
 function ConnectionPool(
-        outbox::Channel = Channel{Any}(DEFAULT_OUTBOX_LIMIT),
+                        outbox::Channel = Any[],
         connections=Set{AbstractConnection}(),
 )
     pool = ConnectionPool(
         outbox,
         connections,
-        Condition(),
     )
-
-    # Catch errors here, otherwise they are lost to the void.
-    @async try
-        process_messages(pool)
-    catch exc
-        @error(
-            "An error ocurred in the while processing messages from a "
-                * "ConnectionPool.",
-            exception=exc,
-        )
-    end
 
     return pool
 end
 
 function addconnection!(pool::ConnectionPool, conn::AbstractConnection)
     push!(pool.connections, conn)
-    notify(pool)
+    process_messages(pool)
 end
 
 function Sockets.send(pool::ConnectionPool, msg)
-    if length(pool.outbox.data) >= pool.outbox.sz_max
-        # TODO: https://github.com/JuliaGizmos/WebIO.jl/issues/343
-        return
-    end
-    put!(pool.outbox, msg)
+    push!(pool.outbox, msg)
+    process_messages(pool)
 end
-
-"""
-    ensure_connection(pool)
-
-Ensure that the pool has at least one connection, potentially blocking the
-current task until that is the case. Also processes incoming connections.
-"""
-function ensure_connection(pool::ConnectionPool)
-    if isempty(pool.connections)
-        wait(pool.condition)
-    end
-end
-
-Base.wait(pool::ConnectionPool) = ensure_connection(pool)
-Base.notify(pool::ConnectionPool) = notify(pool.condition)
 
 """
     process_messages(pool)
@@ -84,9 +48,8 @@ frontends.
 This function should be run as a task (it will block forever otherwise).
 """
 function process_messages(pool::ConnectionPool)
-    ensure_connection(pool)
-    while true
-        msg = take!(pool.outbox)
+    while !isempty(pool.outbox) && !isempty(pool.connections)
+        msg = pop!(pool.outbox)
         @sync begin
             # This may result in sending to no connections, but we're okay with
             # that because we'll just get the next value of the observable
