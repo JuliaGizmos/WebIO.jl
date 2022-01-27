54d96a963a4058aa912f127e8b87e43055fb7d30
rafaqz <rafaelschouten@gmail.com>
2022-01-27 12:51:46 +0100

    fix tests

--- test/http-tests.jl
+++ test/http-tests.jl
@@ -7,6 +7,6 @@ using Test
     @test occursin("_webIOWebSocketURL = ", output)
     @test occursin("ws://127.0.0.1:8081/webio_websocket/", output)
     @test occursin("""hello, world""", output)
-    @test WebIO.webio_server_config[] == (url = "127.0.0.1", bundle_url = WebIO.bundle_key, http_port = 8081, ws_url = "ws://127.0.0.1:8081/webio_websocket/")
+    @test WebIO.webio_server_config[] == (url = "127.0.0.1", bundle_url = WebIO.bundle_key[], http_port = 8081, ws_url = "ws://127.0.0.1:8081/webio_websocket/")
     @test isassigned(WebIO.singleton_instance)
 end
